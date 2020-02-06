99b46471d24a2bbff20c7a7dc3d91df6f45b33b3
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-02-07 01:29:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+const port=process.env.port || 3000;
+
+app.use(express.static(__dirname+'/public'));
+
+
+app.get('/',(req,res)=>{
+    res.sendFile("public/index.html");
+});
+
+app.listen(port);
